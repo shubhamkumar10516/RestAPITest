0078c596b93202816b42f5e5a0a41b03b6332db6
shubhamkumar10516 <shubhamkumar10516@gmail.com>
2024-08-17 22:50:25 +0530

    Added log file

--- src/main/java/com/example/demo/RestApiTestApplication.java
+++ src/main/java/com/example/demo/RestApiTestApplication.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class RestApiTestApplication {
 
+	@PostConstruct
+	public void init() {
+		System.out.println("Application started..");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiTestApplication.class, args);
+		
+	
 	}
 
 }
